cf649b48cf1f7ffa9521d171148d02bd56214631
lpianta <pianta.luca@gmail.com>
2021-05-27 13:10:12 +0200

    first_sequential_model

--- keras_tutorial/first_sequential_model.py
+++ keras_tutorial/first_sequential_model.py
@@ -1,8 +1,14 @@
 # import libraries for data creation and process
+import itertools
 import numpy as np
 from random import randint
 from sklearn.utils import shuffle
 from sklearn.preprocessing import MinMaxScaler
+
+# import for confusion matrix
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import itertools
 
 # import libraries for model creation
 import tensorflow as tf
@@ -22,6 +28,8 @@ from tensorflow.python.ops.gen_nn_ops import softmax
 
 train_samples = []
 train_labels = []  # label0 = no side effects, 1 = side effects
+test_samples = []
+test_labels = []
 
 # FAKE DATA
 # an experimental drug was tested on individuals from age 13 to age 100
@@ -54,17 +62,45 @@ for i in range(1000):
     train_samples.append(random_older)
     train_labels.append(1)
 
+# "outliers" group
+for i in range(50):
+    # the 5% younger individuals who did experience side effect
+    random_younger = randint(13, 64)
+    test_samples.append(random_younger)
+    test_labels.append(1)
+
+    # the 5% older individual who did not experience side effects
+    random_older = randint(65, 100)
+    test_samples.append(random_older)
+    test_labels.append(0)
+
+# "bulk" of the group
+for i in range(1000):
+    # the 95% younger individuals who did not experience side effect
+    random_younger = randint(13, 64)
+    test_samples.append(random_younger)
+    test_labels.append(0)
+
+    # the 95% older individual who did experience side effects
+    random_older = randint(65, 100)
+    test_samples.append(random_older)
+    test_labels.append(1)
+
 # process data to be in the format needed for the fit function
 train_labels = np.array(train_labels)
 train_samples = np.array(train_samples)
+test_labels = np.array(test_labels)
+test_samples = np.array(test_samples)
 
 # shuffling the data
 train_samples, train_labels = shuffle(train_samples, train_labels)
+test_samples, test_labels = shuffle(test_samples, test_labels)
 
 # normalize the data
 scaler = MinMaxScaler(feature_range=(0, 1))
 # we reshape the data because the fit_transform function doesn't accept 1d array
 scaled_train_samples = scaler.fit_transform(train_samples.reshape(-1, 1))
+scaled_test_samples = scaler.fit_transform(test_samples.reshape(-1, 1))
 
 # BUILDING THE MODEL
 model = Sequential([
@@ -91,8 +127,58 @@ model.compile(optimizer=Adam(learning_rate=0.0001),
 # fit function to actually train the model
 model.fit(x=scaled_train_samples,
           y=train_labels,
+          validation_split=0.1,  # defining validation in training
           batch_size=10,
           epochs=30,
-          shuffle=True,
+          shuffle=True,  # the shuffle happens after the splitting, so if we didn't
+          # shuffled before we don't have the validation set shuffled
           verbose=2
           )
+
+# PREDICTION
+# we get the probability for each label
+predictions = model.predict(x=scaled_test_samples,
+                            batch_size=10,
+                            verbose=0)
+
+# argmax to get the highest index of the prediciton probability
+rounded_predictions = np.argmax(predictions, axis=-1)
+
+# CONFUSION MATRIX
+
+cm = confusion_matrix(y_true=test_labels, y_pred=rounded_predictions)
+
+cm_plot_labels = ["no side effects", "side effects"]
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title="Confusion Matrix",
+                          cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation="nearest", cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    else:
+        print("Confusion Matrix, without normalization")
+
+    print(cm)
+
+    thresh = cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel("True Label")
+    plt.xlabel("Predicted Label")
+    plt.show()
+
+
+plot_confusion_matrix(cm=cm, classes=cm_plot_labels)
